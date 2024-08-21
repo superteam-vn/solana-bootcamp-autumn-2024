e94939077532659f7ab9801161ce3ffecdd6570f
leopham <hongthaipro@gmail.com>
2024-08-21 12:40:31 +0700

    update token staking use token 2022

--- week-4/token-2022-staking/programs/token-2022-staking/src/instructions/stake.rs
+++ week-4/token-2022-staking/programs/token-2022-staking/src/instructions/stake.rs
@@ -7,16 +7,21 @@ use crate::{error::MyErrorCode, Pool, StakeInfo, STAKEINFO_SEED};
 
 #[derive(Accounts)]
 pub struct Stake<'info> {
+    // signer is the staker
     #[account(mut)]
     pub signer: Signer<'info>,
+    // check pool account valid
+    // use has_one to check if pool account has a field stake_mint = stake_mint account, if not, return InvalidStakeMintAccount error
     #[account(
       has_one = stake_mint @MyErrorCode::InvalidStakeMintAccount,
     )]
     pub pool: Account<'info, Pool>,
+    // stake_mint token program must be stake_token_program
     #[account(
       mint::token_program = stake_token_program,
     )]
     pub stake_mint: Box<InterfaceAccount<'info, Mint>>,
+    // create a stake_info account to keep the staker's stake information
     #[account(
       init_if_needed,
       payer = signer,
@@ -25,6 +30,7 @@ pub struct Stake<'info> {
       bump
     )]
     pub stake_info: Account<'info, StakeInfo>,
+    // associated token account of the staker to transfer token to stake_info_ata
     #[account(
       mut,
       associated_token::mint = stake_mint,
@@ -32,6 +38,7 @@ pub struct Stake<'info> {
       associated_token::token_program = stake_token_program,
     )]
     pub staker_ata: Box<InterfaceAccount<'info, TokenAccount>>,
+    // associated token account of the stake_info to receive token from staker_ata
     #[account(
       init_if_needed,
       payer = signer,
@@ -55,15 +62,20 @@ impl<'info> Stake<'info> {
     }
 
     fn update_stake_info(&mut self, amount: u64) -> Result<()> {
+      // check if the staker has staked before
+      // if staker has staked before, calculate the reward and add the amount to the stake_info account
+      // then update the stake_info account
       if self.stake_info.amount.gt(&0) {
         self.stake_info.reward = self.pool.calculate_reward(self.stake_info.last_deposit_slot, Clock::get()?.slot);
         self.stake_info.amount = self.stake_info.amount.checked_add(amount).ok_or(MyErrorCode::Overflow)?;
       } else {
+        // else, set the amount and reward to the stake_info account
         self.stake_info.amount = amount;
         self.stake_info.reward = 0;
       }
 
       self.stake_info.staker = self.signer.to_account_info().key();
+      // udate the last_deposit_slot to the current slot to keep track reward
       self.stake_info.last_deposit_slot = Clock::get()?.slot;
       Ok(())
     }
@@ -71,12 +83,20 @@ impl<'info> Stake<'info> {
     fn deposit(&mut self, amount: u64) -> Result<()> {
       require_gt!(amount, 0, MyErrorCode::InvalidDepositAmount);
 
-      transfer_checked(CpiContext::new(self.stake_token_program.to_account_info(), TransferChecked {
-        authority: self.signer.to_account_info(),
-        from: self.staker_ata.to_account_info(),
-        to: self.stake_info_ata.to_account_info(),
-        mint: self.stake_mint.to_account_info(),
-      }),amount, self.stake_mint.decimals)?;
+      // transfer token from staker_ata to stake_info_ata
+      transfer_checked(
+          CpiContext::new(
+            self.stake_token_program.to_account_info(), 
+            TransferChecked {
+              authority: self.signer.to_account_info(),
+              from: self.staker_ata.to_account_info(),
+              to: self.stake_info_ata.to_account_info(),
+              mint: self.stake_mint.to_account_info(),
+            }
+        ), 
+        amount, 
+        self.stake_mint.decimals
+      )?;
 
       Ok(())
     }
--- week-4/token-2022-staking/programs/token-2022-staking/src/instructions/unstake.rs
+++ week-4/token-2022-staking/programs/token-2022-staking/src/instructions/unstake.rs
@@ -5,6 +5,7 @@ use crate::{error::MyErrorCode, Pool, StakeInfo, POOL_SEED, STAKEINFO_SEED};
 
 #[derive(Accounts)]
 pub struct Unstake<'info> {
+    // signer is the staker
     #[account(mut)]
     pub signer: Signer<'info>,
     #[account(
@@ -50,6 +51,7 @@ pub struct Unstake<'info> {
       associated_token::token_program = reward_token_program,   
     )]
     pub reward_ata: Box<InterfaceAccount<'info, TokenAccount>>,
+    // associated token account of the staker to receive reward token
     #[account(
       init_if_needed,
       payer = signer,
@@ -83,32 +85,49 @@ impl<'info> Unstake<'info> {
     let seeds = &[POOL_SEED, self.stake_mint.to_account_info().key.as_ref(), &[bumps.pool]];
     let signer_seeds = &[&seeds[..]];
     let reward_amount = self.stake_info.reward.checked_add(self.pool.calculate_reward(self.stake_info.last_deposit_slot , Clock::get()?.slot)).ok_or(MyErrorCode::Overflow)?; 
+
     transfer_checked(
-      CpiContext::new_with_signer(self.reward_token_program.to_account_info(), 
-       TransferChecked {
-        from: self.reward_ata.to_account_info(),
-        to: self.staker_reward_ata.to_account_info(),
-        authority: self.pool.to_account_info(),
-        mint: self.reward_mint.to_account_info(),
-       }, 
-       signer_seeds
-      ), reward_amount, self.reward_mint.decimals
+      CpiContext::new_with_signer(
+        self.reward_token_program.to_account_info(), 
+        TransferChecked {
+          from: self.reward_ata.to_account_info(),
+          to: self.staker_reward_ata.to_account_info(),
+          authority: self.pool.to_account_info(),
+          mint: self.reward_mint.to_account_info(),
+        }, 
+        signer_seeds
+      ), 
+      reward_amount, 
+      self.reward_mint.decimals
     )?;
 
     Ok(())
   }
 
   fn update_stake_info(&mut self, amount: u64, bumps: &UnstakeBumps) -> Result<()> {
+    //calculate the new stake amount
     let new_amount = self.stake_info.amount.checked_sub(amount).ok_or(MyErrorCode::Overflow)?;
     self.stake_info.amount = new_amount;
     self.stake_info.reward = 0;
     self.stake_info.last_deposit_slot = Clock::get()?.slot;
 
+    // check if staker unstake all tokens
+    // close the stake_info_ata account and stake_info account
+    // return rent fee to the staker
     if new_amount.eq(&0) {
       let seeds = &[STAKEINFO_SEED, self.pool.to_account_info().key.as_ref(), self.signer.to_account_info().key.as_ref(),&[bumps.stake_info]];
       let signer_seeds = &[&seeds[..]];
+
+      // call cpi close spl-token account
       close_account(
-        CpiContext::new_with_signer(self.stake_token_program.to_account_info(), CloseAccount { account: self.stake_info_ata.to_account_info(), destination: self.signer.to_account_info(), authority: self.stake_info.to_account_info() }, signer_seeds)
+        CpiContext::new_with_signer(self.stake_token_program.to_account_info(), 
+        CloseAccount { 
+            account: self.stake_info_ata.to_account_info(), 
+            destination: self.signer.to_account_info(), 
+            authority: self.stake_info.to_account_info() 
+          },
+          signer_seeds
+        )
       )?;
 
       self.stake_info.close(self.signer.to_account_info())?;
@@ -120,16 +139,21 @@ impl<'info> Unstake<'info> {
   fn back_to_staker(&mut self, amount: u64,bumps: &UnstakeBumps) -> Result<()> {
     let seeds = &[STAKEINFO_SEED, self.pool.to_account_info().key.as_ref(), self.signer.to_account_info().key.as_ref(),&[bumps.stake_info]];
     let signer_seeds = &[&seeds[..]];
+
+    // transfer the staked amount to the staker
     transfer_checked(
-      CpiContext::new_with_signer(self.stake_token_program.to_account_info(), 
-       TransferChecked {
-        from: self.stake_info_ata.to_account_info(),
-        to: self.staker_ata.to_account_info(),
-        authority: self.stake_info.to_account_info(),
-        mint: self.stake_mint.to_account_info(),
-       }, 
+      CpiContext::new_with_signer(
+self.stake_token_program.to_account_info(), 
+TransferChecked {
+          from: self.stake_info_ata.to_account_info(),
+          to: self.staker_ata.to_account_info(),
+          authority: self.stake_info.to_account_info(),
+          mint: self.stake_mint.to_account_info(),
+        }, 
        signer_seeds
-      ), amount, self.stake_mint.decimals
+      ), 
+      amount, 
+      self.stake_mint.decimals
     )?;
     Ok(())
   }
